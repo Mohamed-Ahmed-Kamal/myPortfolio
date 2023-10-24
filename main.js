7627d9c94abc700aaae0e1701bccb292420a2e46
mohamedkamal <mk126105@gmail.com>
2023-10-24 03:52:35 +0300

    ..

--- main.js
+++ main.js
@@ -7,4 +7,18 @@ function closedmenu() {
 }
 
 // ------------------------------------------
-
+function sendMail() {
+  Email.send({
+    SecureToken: "1c828b0b-ad3c-4079-8560-b5bbece6c55b",
+    To: `mk126105@gmail.com`,
+    From: "mk126105@gmail.com",
+    Subject: "Test Email",
+    Body:
+      "Name : " +
+      document.getElementById("name").value +
+      "<br> Email: " +
+      document.getElementById("email").value +
+      "<br> Messege: " +
+      document.getElementById("Message").value,
+  }).then((message) => alert("Massage Send Succesfully"));
+}
