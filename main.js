af3ffc5f265d5527ab0bbb99096d18af71faf0dc
mohamedkamal <mk126105@gmail.com>
2023-11-02 23:19:29 +0200

    ...

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+//side bar-----------------
 let sideMenu = document.getElementById("sidemenu");
 function openmenu() {
   sideMenu.style.right = "0";
@@ -6,7 +7,25 @@ function closedmenu() {
   sideMenu.style.right = "-500px";
 }
 
-// -------------------------------------------------------------
+//scroll botton-----------
+let btn = document.getElementById("scroll-bt");
+onscroll = function () {
+  if (scrollY >= 400) {
+    btn.style.display = "block";
+  } else {
+    btn.style.display = "none";
+  }
+};
+
+btn.onclick = function () {
+  scroll({
+    left: 0,
+    top: 0,
+    behavior: "smooth",
+  });
+};
+
+// Email  -------------------------------------------------------------
 function sendMail() {
   Email.send({
     SecureToken: "1c828b0b-ad3c-4079-8560-b5bbece6c55b",
